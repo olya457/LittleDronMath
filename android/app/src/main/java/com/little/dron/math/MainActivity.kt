478e5b5a1420a2f2227ebca3e4837f8bacf534b4
olya <olya0665643972@gmail.com>
2026-05-22 13:00:43 +0300

    init

--- android/app/src/main/java/com/little/dron/math/MainActivity.kt
+++ android/app/src/main/java/com/little/dron/math/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.littledronmath
+package com.little.dron.math
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
--- android/app/src/main/java/com/little/dron/math/MainApplication.kt
+++ android/app/src/main/java/com/little/dron/math/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.littledronmath
+package com.little.dron.math
 
 import android.app.Application
 import com.facebook.react.PackageList
